3a5cbb32ad11590b58da2fc67cd4acaeb128044e
NayaraBento <nayarabentofonsecabraz@gmail.com>
2024-09-04 21:46:35 -0300

    java cadastro

--- Arquivos_JS/cadastro.js
+++ Arquivos_JS/cadastro.js
@@ -10,3 +10,28 @@
     }
 
 })
+let inputCPF= document.getElementById("CPF")
+ inputCPF.addEventListener("input",function(elemento)
+{let value=elemento.target.value 
+    if(value.length ==14){
+        inputCPF.classList.remove("erro")
+        inputCPF.classList.add("correto")
+    } else{
+        inputCPF.classList.remove("correto")
+        inputCPF.classList.add("erro")   
+    }
+    
+     })
+     let inputSenha= document.getElementById("senha")
+     inputSenha.addEventListener("input",function(elemento)
+    {let value=elemento.target.value 
+        if(value.length >=4){
+            inputSenha.classList.remove("erro")
+            inputSenha.classList.add("correto")
+        } else{
+            inputSenha.classList.remove("correto")
+            inputSenha.classList.add("erro")   
+        }
+        
+         })
+    
